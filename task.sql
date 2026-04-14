38ef184159f1dc9ae95458aa0238876f853dc764
yaroslav-demchenko <dastex1411@gmail.com>
2026-04-14 12:43:59 +0300

    added deleting procedure if exists

--- task.sql
+++ task.sql
@@ -1,13 +1,15 @@
 USE ShopDB; 
 
+DROP PROCEDURE IF EXISTS get_warehouse_product_inventory;
+
 DELIMITER //
 CREATE PROCEDURE get_warehouse_product_inventory(
     IN warehouse_id INT
 )
 BEGIN
     SELECT p.Name AS ProductName, pi.WarehouseAmount AS Amount
-    FROM Warehouses w
-    LEFT JOIN ProductInventory pi ON w.ID = pi.WarehouseID
+    FROM ProductInventory pi
+    LEFT JOIN Warehouses w ON pi.WarehouseID = w.ID
     LEFT JOIN Products p ON pi.ProductID = p.ID
     WHERE w.ID = warehouse_id;
 END //
