e400c0ec469b4989456a6b36e264ceb804ad33da
yaroslav-demchenko <dastex1411@gmail.com>
2026-04-14 12:52:28 +0300

    procedure optimization

--- task.sql
+++ task.sql
@@ -9,8 +9,7 @@ CREATE PROCEDURE get_warehouse_product_inventory(
 BEGIN
     SELECT p.Name AS ProductName, pi.WarehouseAmount AS Amount
     FROM ProductInventory pi
-    LEFT JOIN Warehouses w ON pi.WarehouseID = w.ID
     LEFT JOIN Products p ON pi.ProductID = p.ID
-    WHERE w.ID = warehouse_id;
+    WHERE pi.WarehouseID = warehouse_id;
 END //
 DELIMITER ;
